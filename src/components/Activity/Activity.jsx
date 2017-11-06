ceda8d311284d761bab563c97dfb10e66926d360
Maya Balakrishnan <mayanb@gmail.com>
2017-11-05 17:26:12 -0800

    can now get spreadsheets on google drive

--- src/components/Activity/Activity.jsx
+++ src/components/Activity/Activity.jsx
@@ -14,324 +14,347 @@ import ReactImageFallback from "react-image-fallback";
 import Goals from '../Goals/Goals'
 
 export default class Activity extends React.Component {
-  constructor(props) {
-    super(props)
-    this.lastRequestID = -1
-    this.lastTaskRequestID = -1
-    this.state = { 
-      dates: {start: moment(new Date()).format("YYYY-MM-DD"), end: moment(new Date()).format("YYYY-MM-DD")},
-      processes: {},
+	constructor(props) {
+		super(props)
+		this.lastRequestID = -1
+		this.lastTaskRequestID = -1
+		this.state = { 
+			dates: {start: moment(new Date()).format("YYYY-MM-DD"), end: moment(new Date()).format("YYYY-MM-DD")},
+			processes: {},
 
-      expanded: {process: 0, origin: 0},
-      expandedTasks: [],
+			expanded: {process: 0, origin: 0},
+			expandedTasks: [],
 
-      loading: true,
-      taskLoading: false,
-      mini: true
-     }
-  }
+			loading: true,
+			taskLoading: false,
+			mini: true
+		 }
+	}
 
-  componentDidMount() {
-    this.getActivity(this.state.dates)
-  }
+	componentDidMount() {
+		this.getActivity(this.state.dates)
+	}
 
-  handleDateRangeChange(obj) {
-    this.setState({dates: obj})
-    this.getActivity(obj)
-  }
+	handleDateRangeChange(obj) {
+		this.setState({dates: obj})
+		this.getActivity(obj)
+	}
 
-  render() {
+	render() {
 
-    let contentArea = false
-    if (this.state.loading) {
-      contentArea =  <Loading />
-    } else if (!this.state.processes || Object.values(this.state.processes).length == 0) {
-      contentArea = <ZeroState />
-    } else {
-      contentArea = (
-        Object.values(this.state.processes).map(function (process, i) {
-          return <Process key={i} dates={this.state.dates} {...process} expandedTasks={this.state.expandedTasks} onClick={this.handleProcessClick.bind(this)} onOriginClick={this.handleOriginClick.bind(this)} expanded={this.state.expanded.process==process.process_id?this.state.expanded:null}/>
-        }, this)
-      )
-    }
+		let contentArea = false
+		if (this.state.loading) {
+			contentArea =  <Loading />
+		} else if (!this.state.processes || Object.values(this.state.processes).length == 0) {
+			contentArea = <ZeroState />
+		} else {
+			contentArea = (
+				Object.values(this.state.processes).map(function (process, i) {
+					return <Process key={i} dates={this.state.dates} {...process} expandedTasks={this.state.expandedTasks} onClick={this.handleProcessClick.bind(this)} onOriginClick={this.handleOriginClick.bind(this)} expanded={this.state.expanded.process==process.process_id?this.state.expanded:null}/>
+				}, this)
+			)
+		}
 
-    return (
-      <div>
-        <div className={`activity page ${this.state.mini?"mini":""}`}>
+		return (
+			<div>
+				<div className={`activity page ${this.state.mini?"mini":""}`}>
 
-          <Goals />
-        </div>
+					<Goals />
+				</div>
 
-        <div className={`activity page ${this.state.mini?"mini":""}`}>
-          <div className="content">
-            <div className="activity-header page-header">
-              <h2 className="card-header">Activity Log</h2>
-              <div style={{zIndex: 0}}><Datepicker initialDates={this.state.dates} onChange={this.handleDateRangeChange.bind(this)} /></div>
-            </div>
-            {contentArea}
-          </div>
-        </div>
-      </div>
-    )
-  }
+				<div className={`activity page ${this.state.mini?"mini":""}`}>
+					<div className="content">
+						<div className="activity-header page-header">
+							<h2 className="card-header">Activity Log</h2>
+							<div style={{zIndex: 0}}><Datepicker initialDates={this.state.dates} onChange={this.handleDateRangeChange.bind(this)} /></div>
+						</div>
+						{contentArea}
+					</div>
+				</div>
+			</div>
+		)
+	}
 
-  handleOriginClick(process, origin) {
-    if (process == this.state.expanded.process && origin == this.state.expanded.origin) {
-      this.setState({expanded: {process: process, origin: -1}})
-    } else {
-      this.setState({expanded: {process: process, origin: origin}})
-      this.getTasks(this.state.processes[process], this.state.processes[process].origins[origin])
-    }
-  }
+	handleOriginClick(process, origin) {
+		if (process == this.state.expanded.process && origin == this.state.expanded.origin) {
+			this.setState({expanded: {process: process, origin: -1}})
+		} else {
+			this.setState({expanded: {process: process, origin: origin}})
+			this.getTasks(this.state.processes[process], this.state.processes[process].origins[origin])
+		}
+	}
 
-  handleProcessClick(process) {
-    if (process == this.state.expanded.process) {
-      this.setState({expanded: {process: -1, origin: -1}})
-    } else {
-      this.setState({expanded: {process: process, origin: -1}})
-    }
-  }
+	handleProcessClick(process) {
+		if (process == this.state.expanded.process) {
+			this.setState({expanded: {process: -1, origin: -1}})
+		} else {
+			this.setState({expanded: {process: process, origin: -1}})
+		}
+	}
 
-  getTasks(process, origin) {
-    let range = this.state.dates
-    this.setState({taskLoading: true})
-    let processID = process.process_id
-    let productID = origin.product_id
+	getTasks(process, origin) {
+		let range = this.state.dates
+		this.setState({taskLoading: true})
+		let processID = process.process_id
+		let productID = origin.product_id
 
-    let params = {
-      process_type: processID, 
-      product_type: productID, 
-      start: toUTCString(range.start), 
-      end: toUTCString(range.end, true)
-    }
+		let params = {
+			process_type: processID, 
+			product_type: productID, 
+			start: toUTCString(range.start), 
+			end: toUTCString(range.end, true)
+		}
 
-    let url = "/ics/activity/detail/"
-    let component = this
+		let url = "/ics/activity/detail/"
+		let component = this
 
-    let rID = requestID()
-    this.lastTaskRequestID = rID
+		let rID = requestID()
+		this.lastTaskRequestID = rID
 
-    console.log(params.start)
-    console.log(params.end)
+		console.log(params.start)
+		console.log(params.end)
 
-    api.get(url)
-      .query(`start=${params.start}&end=${params.end}&process_type=${processID}&product_type=${productID}`)
-      .end(function (err, res) {
-        console.log(res)
-        if (err || !res.ok) {
-          console.log(err)
-          component.setState({taskLoading: false})
-          return
-        }
-        if (component.lastTaskRequestID != rID) 
-          return
-        component.setState({expandedTasks: res.body, taskLoading: false})
-      })
-  }
+		api.get(url)
+			.query(`start=${params.start}&end=${params.end}&process_type=${processID}&product_type=${productID}`)
+			.end(function (err, res) {
+				console.log(res)
+				if (err || !res.ok) {
+					console.log(err)
+					component.setState({taskLoading: false})
+					return
+				}
+				if (component.lastTaskRequestID != rID) 
+					return
+				component.setState({expandedTasks: res.body, taskLoading: false})
+			})
+	}
 
-  getActivity(range) {
-    this.setState({loading: true})
-    let url = "/ics/activity/"
-    let params = {start: toUTCString(range.start), end: toUTCString(range.end, true)}
-    let component = this
+	getActivity(range) {
+		this.setState({loading: true})
+		let url = "/ics/activity/"
+		let params = {start: toUTCString(range.start), end: toUTCString(range.end, true)}
+		let component = this
 
-    let rID = requestID()
-    this.lastRequestID = rID
+		let rID = requestID()
+		this.lastRequestID = rID
 
-    api.get("/ics/activity/")
-      .query(params)
-      .end(function (err, res) {
-        let data = res.body
-        if (component.lastRequestID != rID) 
-          return
+		api.get("/ics/activity/")
+			.query(params)
+			.end(function (err, res) {
+				let data = res.body
+				if (component.lastRequestID != rID) 
+					return
 
-        if (err || !res.ok)
-          alert("ugh something went wrong\n" + err)
+				if (err || !res.ok)
+					alert("ugh something went wrong\n" + err)
 
-        component.lastTaskRequestID = -1
-        component.setState({
-          processes: component.nestProcesses(data), 
-          expanded: {process:-1, origin:-1},
-          expandedTasks: [],
-          taskLoading: false, loading: false
-        })
-      })
-      // .end(function (err, res) {
-      //   if (component.lastRequestID != rID) 
-      //     return
-      //   component.setState({taskLoading: false, loading: false})
-      // })
-  }
+				component.lastTaskRequestID = -1
+				component.setState({
+					processes: component.nestProcesses(data), 
+					expanded: {process:-1, origin:-1},
+					expandedTasks: [],
+					taskLoading: false, loading: false
+				})
+			})
+			// .end(function (err, res) {
+			//   if (component.lastRequestID != rID) 
+			//     return
+			//   component.setState({taskLoading: false, loading: false})
+			// })
+	}
 
-  nestProcesses(data) {
-    let processes = {}
-    for (var s in data) {
-      let skew = data[s]
-      let pid = skew.process_id
-      if(!processes[pid]) {
-        processes[pid] = update({}, {$merge: skew})
-        processes[pid].origins = []
-        processes[pid].runs = 0
-        processes[pid].outputs = 0
-      }
-      processes[pid].runs += parseInt(skew.runs)
-      processes[pid].outputs += parseInt(skew.outputs)
-      processes[pid].origins.push(skew)
-    }
-    return processes
-  }
+	nestProcesses(data) {
+		let processes = {}
+		for (var s in data) {
+			let skew = data[s]
+			let pid = skew.process_id
+			if(!processes[pid]) {
+				processes[pid] = update({}, {$merge: skew})
+				processes[pid].origins = []
+				processes[pid].runs = 0
+				processes[pid].outputs = 0
+			}
+			processes[pid].runs += parseInt(skew.runs)
+			processes[pid].outputs += parseInt(skew.outputs)
+			processes[pid].origins.push(skew)
+		}
+		return processes
+	}
 
 }
 
 
+
+function attemptSpreadsheet(params) {
+	let is_connected = api.get_active_user().user.has_gauth_token
+	if (is_connected) {
+		createSpreadsheet(params)
+	}
+	else {
+		alert("You must first connect a Google account in account settings\n")
+	}
+}
+
 function createSpreadsheet(params) {
-  console.log("creating spreadsheet")
-  api.post('/gauth/create-spreadsheet/')
-    .type('form')
-    .send(params)
-    .end(function (err, res) {
-      console.log(res)
-      // window.location.href = res.text
-    })
+	console.log("creating spreadsheet")
+	api.post('/gauth/create-spreadsheet/')
+		.type('form')
+		.send(params)
+		.end(function (err, res) {
+			if (err || !res.ok) {
+				alert("ugh something went wrong\n" + err)				
+			}
+			else {
+				console.log(res.body.spreadsheetId)
+				let url = 'https://docs.google.com/spreadsheets/d/' + res.body.spreadsheetId + '/'
+				var newWin = window.open(url, '_blank');
+				if(!newWin || newWin.closed || typeof newWin.closed=='undefined') 
+				{
+					//POPUP BLOCKED
+					alert('Your spreadsheet was created in Google Sheets. To view your new sheet in a tab, you must select: always allow pop-ups from this webpage')
+				}
+			}
+			// window.location.href = res.text
+		})
 }
 
 
 function Process(props) {
-  let origins = false
+	let origins = false
 
 
-  if (props.expanded != null) {
-    origins = props.origins.map(function (origin, i) {
-      return <Origin 
-        key={i} 
-        {...origin} 
-        onClick={() => props.onOriginClick(props.process_id, i)} 
-        process_unit={props.process_unit} 
-        expanded={props.expanded.origin==i}
-        expandedTasks={props.expandedTasks}
-      />
-    }, this)
-  }
+	if (props.expanded != null) {
+		origins = props.origins.map(function (origin, i) {
+			return <Origin 
+				key={i} 
+				{...origin} 
+				onClick={() => props.onOriginClick(props.process_id, i)} 
+				process_unit={props.process_unit} 
+				expanded={props.expanded.origin==i}
+				expandedTasks={props.expandedTasks}
+			/>
+		}, this)
+	}
 
-  let user_id = api.get_active_user().user.user_id
+	let user_id = api.get_active_user().user.user_id
 
-  let button = <button 
-    onClick={(e) => createSpreadsheet({"user_id": user_id, "process": props.process_id, "start": toUTCString(props.dates.start), "end": toUTCString(props.dates.end, true)})}
-  ><i className="material-icons" >file_download</i></button>
+	let button = <button 
+		onClick={(e) => attemptSpreadsheet({"user_id": user_id, "process": props.process_id, "start": toUTCString(props.dates.start), "end": toUTCString(props.dates.end, true)})}
+	><i className="material-icons" >file_download</i></button>
 
-  return (
-    <div className={ "activity-process " + (props.expanded?"expanded":"")}> 
-      <div onClick={() => props.onClick(props.process_id)}>
-        <Row className="activity-process-header"
-          img={props.process_name.toLowerCase().replace(/\s/g, '')}
-          first={props.process_code}         
-          second={props.process_name}
-          third={pl(props.runs, "run")}
-          fourth={pl(parseInt(props.outputs), props.process_unit)}
-          fifth={"0 flagged"}
-          sixth={"0 experimental"}
-          seventh={<div />}
+	return (
+		<div className={ "activity-process " + (props.expanded?"expanded":"")}> 
+			<div onClick={() => props.onClick(props.process_id)}>
+				<Row className="activity-process-header"
+					img={props.process_name.toLowerCase().replace(/\s/g, '')}
+					first={props.process_code}         
+					second={props.process_name}
+					third={pl(props.runs, "run")}
+					fourth={pl(parseInt(props.outputs), props.process_unit)}
+					fifth={"0 flagged"}
+					sixth={"0 experimental"}
+					seventh={button}
 
-        />
-      </div>
-      {origins}
-    </div>
-  )
+				/>
+			</div>
+			{origins}
+		</div>
+	)
 }
 
 function Origin(props) {
-  var taskList = false
-  if (props.expanded) {
-    taskList = props.expandedTasks.map(function (task) {
-      return <TaskList {...task} process_unit={props.process_unit}/>
-    })
-  }
-  return (
-    <div className={props.expanded?"expanded-origin":""}>
-      <div onClick={props.onClick}>
-        <Row className="process-origin-header"
-          icon={props.expanded?"expand_more":"chevron_right"} 
-          first={props.product_code}
-          second={pl(props.runs, "run")}
-          third={pl(parseInt(props.outputs), props.process_unit)}
-          fourth={!props.flagged?"--":props.flagged + " flagged"}
-          fifth={!props.experimental?"--":props.experimental + " experimental"}
-        />
-      </div>
-      { taskList }
-    </div>
-  )
+	var taskList = false
+	if (props.expanded) {
+		taskList = props.expandedTasks.map(function (task) {
+			return <TaskList {...task} process_unit={props.process_unit}/>
+		})
+	}
+	return (
+		<div className={props.expanded?"expanded-origin":""}>
+			<div onClick={props.onClick}>
+				<Row className="process-origin-header"
+					icon={props.expanded?"expand_more":"chevron_right"} 
+					first={props.product_code}
+					second={pl(props.runs, "run")}
+					third={pl(parseInt(props.outputs), props.process_unit)}
+					fourth={!props.flagged?"--":props.flagged + " flagged"}
+					fifth={!props.experimental?"--":props.experimental + " experimental"}
+				/>
+			</div>
+			{ taskList }
+		</div>
+	)
 }
 
 function Row(props) {
-  var symbol = false 
-  if (props.img) {
-    let errorImg = `${process.env.PUBLIC_URL}/public/img/default@3x.png`
-    symbol = <ReactImageFallback src={process.env.PUBLIC_URL + `/img/${props.img}@3x.png`} fallbackImage={errorImg}/>
-  } else if (props.icon) {
-    symbol = <span><i className="material-icons arrow">{props.icon}</i></span>
-  }
+	var symbol = false 
+	if (props.img) {
+		let errorImg = `${process.env.PUBLIC_URL}/public/img/default@3x.png`
+		symbol = <ReactImageFallback src={process.env.PUBLIC_URL + `/img/${props.img}@3x.png`} fallbackImage={errorImg}/>
+	} else if (props.icon) {
+		symbol = <span><i className="material-icons arrow">{props.icon}</i></span>
+	}
 
-  return (
-    <div className={"activity-process-row " + props.className} >
-      <div className={"min " + (props.img?"img":"")}>
-        {symbol}
-      </div>
-      <div className="process-name">
-        <span>{props.first}</span>
-      </div>
-      <div className="process-name">
-        <span>{props.second}</span>
-      </div>
-      <div className="process-runs tiny">
-        <span>{props.third}</span>
-      </div>
-      <div className="process-outputs">
-        <span>{props.fourth}</span>
-      </div>
-      <div className="process-flagged tiny">
-        <span>{props.fifth}</span>
-      </div>
-      <div className="process-experimental tiny">  
-        <span>{props.sixth}</span>
-      </div>
-      <div className="process-button no">
-        <span>{props.seventh}</span>
-      </div>
-    </div>
-  )
+	return (
+		<div className={"activity-process-row " + props.className} >
+			<div className={"min " + (props.img?"img":"")}>
+				{symbol}
+			</div>
+			<div className="process-name">
+				<span>{props.first}</span>
+			</div>
+			<div className="process-name">
+				<span>{props.second}</span>
+			</div>
+			<div className="process-runs tiny">
+				<span>{props.third}</span>
+			</div>
+			<div className="process-outputs">
+				<span>{props.fourth}</span>
+			</div>
+			<div className="process-flagged tiny">
+				<span>{props.fifth}</span>
+			</div>
+			<div className="process-experimental tiny">  
+				<span>{props.sixth}</span>
+			</div>
+			<div className="process-button no">
+				<span>{props.seventh}</span>
+			</div>
+		</div>
+	)
 }
 
 function TaskList(props) {
-  return (
-    <div className="task-list">
-    <Row className="process-origin-task"
-      first={" "}
-      second={<a href={window.location.origin + "/task/" + props.id} target="_blank">{display(props)}</a>}
-      third={pl(parseInt(props.outputs), props.process_unit)}
-      fourth={"--"}
-      fifth={"--"}
-    />
-    </div>
-  )
+	return (
+		<div className="task-list">
+		<Row className="process-origin-task"
+			first={" "}
+			second={<a href={window.location.origin + "/task/" + props.id} target="_blank">{display(props)}</a>}
+			third={pl(parseInt(props.outputs), props.process_unit)}
+			fourth={"--"}
+			fifth={"--"}
+		/>
+		</div>
+	)
 }
 
 function pl(count, unit) {
-  if (count) {
+	if (count) {
 
-  }
-  if (count == 1) 
-    return count + " " + unit
-  return count + " " + unit + "s"
+	}
+	if (count == 1) 
+		return count + " " + unit
+	return count + " " + unit + "s"
 }
 
 
 function toUTCString(dateString, addOne) {
-  var m = moment(dateString + " 00:00:00")
+	var m = moment(dateString + " 00:00:00")
 
-  if (addOne) {
-    m.add(24, "hours")
-  }
+	if (addOne) {
+		m.add(24, "hours")
+	}
 
-  return m.utc().format('YYYY-MM-DD-HH-mm-ss-SSSSSS')
+	return m.utc().format('YYYY-MM-DD-HH-mm-ss-SSSSSS')
 }
