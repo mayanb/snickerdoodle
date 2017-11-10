61ea99f1b1f45fd93885126256b26392983c672b
Maya Balakrishnan <mayanb@gmail.com>
2017-11-09 16:40:55 -0800

    created packing order files

---
+++ src/components/PackingOrders/PackingOrders.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import * as actions from './PackingOrdersActions.jsx'
+import Card from '../Card/Card.jsx'
+
+class PackingOrders extends React.Component {
+
+   // fetch movements on load
+  componentDidMount() {
+    this.props.dispatch(actions.fetchPackingOrders())
+  }
+
+
+  render() {
+  	let packingOrders = this.props.packingOrders
+  	if (this.props.isFetching === true) {return(<div>hi</div>)}
+
+  	return(
+  		<ul>
+  			{packingOrders.map(function(packingOrder, index) {
+  				return(<li>{JSON.stringify(packingOrder)}</li>)
+  			})}
+  		</ul>
+  	)
+  }
+
+
+}
+
+// This is our select function that will extract from the state the data slice we want to expose
+// through props to our component.
+const mapStateToProps = (state/*, props*/) => {
+  console.log(state)
+  return {
+    movements: state.movements.data,
+    isFetching: state.movements.ui.isFetchingData
+  }
+}
+
+const connectedMovements = connect(mapStateToProps)(Movements)
+
+export default connectedMovements
---
+++ src/components/PackingOrders/PackingOrdersActions.jsx
@@ -0,0 +1,53 @@
+import api from '../WaffleconeAPI/api.jsx'
+import {
+  REQUEST, 
+  REQUEST_SUCCESS, 
+  REQUEST_FAILURE,
+} from '../../Reducers/APIDataReducer'
+import {  MOVEMENTS } from '../../Reducers/ReducerTypes'
+
+
+export function fetchMovements() {
+  return function (dispatch) {
+    // dispatch an action that we are requesting a process
+    dispatch(requestMovements())
+
+    // actually fetch 
+    return api.get('/ics/processes/')
+      .end( function (err, res) {
+        if (err || !res.ok) {
+          dispatch(requestMovementsFailure(err))
+        } else {
+          let movements = res.body
+          dispatch(requestMovementsSuccess(movements))
+        }
+      })
+  }
+}
+
+function requestMovements() {
+  console.log("requesting movements")
+  return {
+    name: MOVEMENTS,
+    type: REQUEST
+  }
+}
+
+function requestMovementsFailure(err) {
+  alert('Oh no! Something went wrong\n' + err)
+  return {
+    name: MOVEMENTS,
+    type: REQUEST_FAILURE, 
+    error: err
+  }
+}
+
+function requestMovementsSuccess(json) {
+  return {
+    name: MOVEMENTS,
+    type: REQUEST_SUCCESS, 
+    data: json
+  }
+}
+
+
