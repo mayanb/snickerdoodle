9a49bcd58f0a0d4710a9bc5ad47ebfce77ac82fb
Ishita Prasad <ishita@usepolymer.com>
2017-11-09 17:48:38 -0800

    dialog

--- src/components/PackingOrders/PackingOrdersCreateDialog.jsx
+++ src/components/PackingOrders/PackingOrdersCreateDialog.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Button from '../Card/Button'
+import Dialog from '../Card/Dialog'
 
-export class PackingOrdersCreateDialog extends React.Component {
+export default class PackingOrdersCreateDialog extends React.Component {
 	render() {
 		return (
-			<Dialog />
+			<Dialog onToggle={this.props.onToggle}>
+				<h2>Create a new packing order</h2>
+
+				<div className="button-area">
+					<Button secondary onClick={this.props.onToggle}>Cancel</Button>
+					<Button onClick={() => {}}>Create</Button>
+				</div>
+			</Dialog>
 		)
 	}
 }
