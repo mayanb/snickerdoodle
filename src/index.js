7af26b66cf4a465367740d3ae9daca41b800e0c0
Aaron Brown <aaron@usepolymer.com>
2017-11-27 13:22:56 -0800

    Add normalize.css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'normalize.css';
 import './index.css';
 import Application from './components/Application/Application.jsx';
 import createStore from './create-store.jsx'
