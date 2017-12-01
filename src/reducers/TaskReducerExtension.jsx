d7b704aa77d96567ef29fb7dd80de7e9558c8682
Ishita Prasad <ishita@usepolymer.com>
2017-12-01 15:34:21 -0800

    folder name lowercase

---
+++ src/reducers/TaskReducerExtension.jsx
@@ -0,0 +1,52 @@
+import update from 'immutability-helper'
+import { apiDataReducer } from './APIDataReducer'
+import { _taskAttribute } from './TaskAttributeReducerExtension'
+
+export const MARK_OUTPUT_USED = 'MARK_OUTPUT_USED'
+export const REQUEST_EDIT_TASK = 'REQUEST_EDIT_TASK'
+export const REQUEST_EDIT_TASK_SUCCESS = 'REQUEST_EDIT_TASK_SUCCESS'
+
+export function _task(state, action) {
+	let ns = apiDataReducer(state, action)
+  ns = _taskAttribute(ns, action)
+
+	switch (action.type) {
+	  case MARK_OUTPUT_USED:
+    	return markOutputUsed(ns, action)
+    case REQUEST_EDIT_TASK:
+    	//return requestEditTask(ns, action)
+    case REQUEST_EDIT_TASK_SUCCESS:
+    	return requestEditTaskSuccess(ns, action)
+    default:
+      return ns
+  }
+}
+
+
+function markOutputUsed(state, action) {
+  let index = action.index
+  return update(state, {
+    data: {
+    	items: {
+    		[action.index]: {
+    			$merge: {is_used: true}
+    		}
+    	}
+    }
+  })
+}
+
+function requestEditTaskSuccess(state, action) {
+  return update(state, {
+    ui: {
+      isEditingItem: {
+        $set: false
+      },
+    },
+    data: {
+      $merge: {
+        [action.field]: action.value
+      }
+    },
+  })
+}
