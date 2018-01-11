a67a751fd4bbe44ff68bf68d80476550f425c939
Steven <steven@Stevens-MacBook-Pro.local>
2018-01-10 16:48:10 -0800

    Set backend based on environment variables

--- src/components/WaffleconeAPI/urls.jsx
+++ src/components/WaffleconeAPI/urls.jsx
@@ -1,6 +1,13 @@
-export const PRODUCTION_BACKEND = 'https://41aty886e1.execute-api.us-west-1.amazonaws.com/production'
-export const STAGING_BACKEND = 'https://eszlr18ifi.execute-api.us-west-1.amazonaws.com/staging'
-export const LOCAL_BACKEND = 'http://127.0.0.1:8000'
+export function getBackend() {
+	switch(process.env.REACT_APP_BACKEND) {
+		case 'production':
+			return 'https://41aty886e1.execute-api.us-west-1.amazonaws.com/production'
+		case 'staging':
+			return 'https://eszlr18ifi.execute-api.us-west-1.amazonaws.com/staging'
+		default:
+			return 'http://127.0.0.1:8000'
+	}
+}
 
 export function latest(host, path) {
 	let url = host + path
