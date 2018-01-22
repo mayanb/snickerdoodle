4a5f32fc86c803c66e4eefa6774c64c1f0c2b3af
Ishita Prasad <ishita@usepolymer.com>
2018-01-22 15:44:08 -0800

    merge fixed

--- src/components/Application/ApplicationLayout.jsx
+++ src/components/Application/ApplicationLayout.jsx
@@ -2,14 +2,8 @@ import React from 'react'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import './styles/layout.css'
 import Walkthrough from '../Walkthrough/Walkthrough'
-<<<<<<< HEAD
 import CreateTeam from '../Walkthrough/CreateTeam'
-//import Activity from '../Activity/Activity'
 import Home from '../Home/Home'
-=======
-//import Activity from '../Activity/Activity'
-//import Home from '../Home/Home'
->>>>>>> d601a50c53d1fb115a0957340d02982ddb3a2ed5
 import Processes from '../Processes/Processes'
 import Products from '../Products/Products'
 
@@ -42,7 +36,7 @@ export default class ApplicationLayoutStable extends React.Component {
       <Router>
 
           <Switch>
-            <Route path="/create" component={CreateTeam} />
+            <Route path="/create/:code?" component={CreateTeam} />
             <Route path={"/introduction"} component={Walkthrough} />
           	<Route path="/login" component={Login} />
           	<PrivateRoute component={App}/>
--- src/components/Walkthrough/CreateTeam.jsx
+++ src/components/Walkthrough/CreateTeam.jsx
@@ -1,10 +1,49 @@
 import React from 'react'
 import WalkthroughCreateUser from './WalkthroughCreateUser'
+import api from '../WaffleconeAPI/api'
 
-export default function CreateTeam() {
-	return (
-		<div className="walkthrough">
-				<WalkthroughCreateUser />
+const FETCHING = 'f'
+const INVALID = 'i'
+const VALID = 'v'
+
+export default class CreateTeam extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			code_state: FETCHING,
+		}
+	}
+
+	componentDidMount() {
+		api.get('ics/use-code')
+			.query({code: this.props.match.params.code})
+			.then((res) => {
+				let validity = this.validateCodeResponse(res.body) ? VALID : INVALID
+				this.setState({valid_code: validity})
+			})
+			.catch((e) => this.setState({valid_code: INVALID}))
+	}
+
+	validateCodeResponse(data) {
+		return (data && data.length && !data.is_used)
+	}
+
+	render() {
+		if (this.state.code_state === INVALID) {
+			this.props.history.push('/')
+			return null
+		}
+
+		let content = <div>Loading...</div>
+		if (this.state.code_state === VALID) {
+			content = <WalkthroughCreateUser />
+		}
+
+		return (
+			<div className="walkthrough">
+				{content}
 			</div>
-	)
+		)
+	}
+
 }
