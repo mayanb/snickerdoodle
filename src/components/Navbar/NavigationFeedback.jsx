5d83d8a9636bee116a12675c4813bf610cf79e78
Steven Guichard <steven@usepolymer.com>
2018-02-05 11:24:13 -0800

    Open Getting Started Guide in new tab

--- src/components/Navbar/NavigationFeedback.jsx
+++ src/components/Navbar/NavigationFeedback.jsx
@@ -6,7 +6,11 @@ export default function NavigationFeedback(props) {
 			<span>Bugs? Questions?</span>
 			<a href="https://goo.gl/forms/h3uINKz62Q5bN8qP2" target="_blank">Talk to us.</a>
 			<span>Or visit the </span>
-			<a href="https://polymer-publications.gitbooks.io/getting-started/content/">Getting Started guide.</a>
+			<a href="https://polymer-publications.gitbooks.io/getting-started/content/"
+			   target="_blank">
+				Getting Started guide.
+				<i className="material-icons expand-i">open_in_new</i>
+			</a>
 		</div>
 	)
 }
