b0f573ba1729fdf1faa2ccf12a8f14dcaaa514a0
Steven Guichard <steven@usepolymer.com>
2018-02-07 12:51:08 -0800

    Updated styling for change email dialog

--- src/components/Account/AccountDetailsEdit.jsx
+++ src/components/Account/AccountDetailsEdit.jsx
@@ -1,71 +1,58 @@
 import React from 'react'
-import Dialog from '../Card/Dialog'
-import Button from '../Card/Button'
-import DialogHeader from '../Dialog/DialogHeader'
-import DialogFooter from '../Dialog/DialogFooter'
-
-let editor_wrapper_style = { display: "flex", marginBottom: "16px"}
-let span_style = { display: "block", flex: 3, lineHeight: "28px" }
-let input_style = { flex: 5 }
-let input_input_style = {width: "100%"}
-let footer_style = {display: 'flex', justifyContent: 'flex-end'}
-let invalid_style = {color: 'red'}
+import FormDialog from '../FormDialog/FormDialog'
+import FormGroup from '../Inputs/FormGroup'
+import FormErrors from '../Inputs/FormErrors'
+import Input from '../Inputs/Input'
 
 export default class AccountDetailsEdit extends React.Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			value: props.initialValue || "", 
-			valid: true
+			value: props.initialValue || "",
+			submitted: false
 		}
+
+		this.onSubmit = this.onSubmit.bind(this)
 	}
 
 	renderValidation() {
-		if (this.state.valid) 
-			return 
-		return <span style={invalid_style}>Please enter a valid email address.</span>
+		if (this.state.submitted)
+			return <FormErrors errors={this.formErrors()} />
 	}
 
 	render() {
 		let props = this.props
 		let display = props.keyDisplay.toLowerCase()
 		return (
-			<Dialog onToggle={props.onCancel}>
-				<DialogHeader>{`Change ${display}`}</DialogHeader>
-				<div style={editor_wrapper_style}>
-					<span style={span_style}>{`New ${display}`}</span>
-					<div style={input_style}>
-						<input style={input_input_style} 
-							placeholder="john@smith.com" 
-							value={this.state.value} 
-							onChange={(e) => this.setState({value: e.target.value})} 
-						/>
-						{ this.renderValidation() }
-					</div>
-				</div>
-				<DialogFooter>
-					<div style={footer_style}>
-						<Button secondary onClick={props.onCancel}>Cancel</Button>
-						<Button onClick={this.onSubmit.bind(this)}>Save</Button>
-					</div>
-				</DialogFooter>
-			</Dialog>
+			<FormDialog
+				onToggle={props.onCancel}
+				onSave={this.onSubmit}
+				title={`Change ${display}`}
+			>
+				<FormGroup label={`New ${display}`}>
+					<Input
+						placeholder="john@smith.com"
+						value={this.state.value}
+						onChange={(e) => this.setState({ value: e.target.value })}
+					/>
+				</FormGroup>
+				{this.renderValidation()}
+			</FormDialog>
 		)
 	}
 
 	onSubmit() {
-		let callback = () => {
-			if (this.state.valid)
-				this.props.onSubmit(this.state.value)
-		}
-
-		this.setState({valid: this.validate()}, callback)
+		this.setState({ submitted: true })
+		if (this.formErrors().length === 0)
+			this.props.onSubmit(this.state.value)
 	}
 
-	validate() {
-		if (this.props.keyword === 'email') {
-			return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.state.value) 
-		}
-		return true
+	formErrors() {
+		const errors = []
+		const EMAIL_REGEX = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+		if (this.props.keyword === 'email' && !EMAIL_REGEX.test(this.state.value))
+			errors.push('Please enter a valid email address.')
+		return errors
 	}
 }
