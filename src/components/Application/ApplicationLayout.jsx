d6989f5890cf71eca8d279d845f76a26fe10c09f
Steven Guichard <steven@usepolymer.com>
2018-03-13 11:03:04 -0700

    Update nav bar links

--- src/components/Application/ApplicationLayout.jsx
+++ src/components/Application/ApplicationLayout.jsx
@@ -65,7 +65,8 @@ function App(props) {
       <div className="application-content">
         <Route exact path={"/"} component={Home} />
 	      <Route exact path={"/activity-log"} component={Activity} />
-        <Route exact path={"/inventory/:id?"} component={Inventory} />
+	      <Route path="/inventory" component={Inventory2} />
+        <Route exact path={"/old-inventory"} component={Inventory} />
         <Route exact path={"/labels/"} component={ZebraPrinter} />
         <Route path={"/zebra/"} component={ZebraPrinter} />
         <Route path={"/dymo/"} component={LabelPrinter} />
@@ -81,7 +82,6 @@ function App(props) {
         <Route path={"/googleconnect/:ext?"} component={Account} />
         <Route path={"/bc/"} component={BCWrapper} />
         <Route path={"/barcharttest/"} component={Sortable} />
-        <Route path="/pagination-test" component={Inventory2} />
       </div>
     </div>
   )
