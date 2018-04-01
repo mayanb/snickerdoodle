449f1bf2e18c763f84cf4dfad46755238a182012
Ishita Prasad <ishita@usepolymer.com>
2018-04-01 16:36:49 -0700

    styling

--- src/components/ProcessPage/ProcessPageHeader.jsx
+++ src/components/ProcessPage/ProcessPageHeader.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ApplicationSectionHeader from '../Application/ApplicationSectionHeader'
 
-export default function ProcessPageHeader({processName}) {
+export default function ProcessPageHeader({processName, history}) {
 	return (
 		<ApplicationSectionHeader>
 			<div className="process-page-header">
-				<i className="material-icons">arrow_back</i>
+				<i className="material-icons" onClick={() => history.push('/processes')}>arrow_back</i>
 				<span>{`Processes / ${processName}`}</span>
 			</div>
 		</ApplicationSectionHeader>
