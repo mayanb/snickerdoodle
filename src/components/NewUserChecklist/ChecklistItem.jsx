9c654413e2ed19cc92a2b37e43ee95c81ae33e71
Ishita Prasad <ishita@usepolymer.com>
2018-04-02 20:15:15 -0700

    green checkboxes, links work

--- src/components/NewUserChecklist/ChecklistItem.jsx
+++ src/components/NewUserChecklist/ChecklistItem.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './styles/checklistitem.css'
 
-export default function ChecklistItem({ header, text, link, isDone }) {
+export default function ChecklistItem({ header, text, link, isDone, onClick }) {
 	return (
-		<div className={"checklist-item " + (isDone && " checklist-item-done")}>
+		<div className={"checklist-item " + (isDone && " checklist-item-done")} onClick={onClick}>
 			<div className="checklist-item-circle">
 				<div>
 					{isDone && <i className="material-icons">check_circle</i>}
