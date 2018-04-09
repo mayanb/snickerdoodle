f9ef984b7d14d2691f77f91c9727cb920d13e9b0
Steven Guichard <steven@usepolymer.com>
2018-04-09 11:13:34 -0700

    Remove unused Old Components

--- src/components/OldComponents/Task.jsx
+++ src/components/OldComponents/Task.jsx
@@ -4,41 +4,6 @@ function display(task) {
 	return words(task).toUpperCase()
 }
 
-function getNotes(task) {
-  var notesID = 0
-  for (var attribute of task.process_type.attributes) {
-    if(attribute.name.toLowerCase().trim() === "notes") {
-      notesID = attribute.id
-      break;
-    }
-  }
-
-  for (var attributeVal of task.attribute_values) {
-    if(attributeVal.attribute === notesID)
-      return attributeVal.value
-  }
-
-  return ""
-}
-
-function getOperator(task) {
-  var notesID = 0
-  for (var attribute of task.process_type.attributes) {
-    if(attribute.name.toLowerCase().trim() === "operator") {
-      notesID = attribute.id
-      break;
-    }
-  }
-
-  for (var attributeVal of task.attribute_values) {
-    if(attributeVal.attribute === notesID)
-      return attributeVal.value
-  }
-
-  return ""
-}
-
-
 function words(task) {
   if (!task || task === undefined || task.label === undefined) {
   	return ""
@@ -52,52 +17,9 @@ function words(task) {
     return task.label
 }
 
-
-function getAttributesToColumnNumbers(attributes) {
-	var cols = {}
-	attributes.forEach(function (a, i) {
-		cols[a.id] = i
-	})
-	return cols
-}
-
-function taskAsRow(process,task, cols) {
-	var arr = [display(task), '' + task.inputs.length, '' + task.items.length, '' + moment(task.created_at).format("MM/DD/YYYY")]
-	
-	var attrArray = Array(process.attributes.length).fill('')
-	
-	task.attribute_values.forEach(function (av) {
-		var col = cols[av.attribute]
-		attrArray[col] = av.value.replace(/"/g, '""');
-	})
-
-	arr = arr.concat(attrArray)
-
-	return '"' + arr.join('","') + '"'
-}
-
-function toCSV(process, tasks) {
-
-	var attributeColumns = getAttributesToColumnNumbers(process.attributes)
-
-	var arr = ['TaskPage', 'Inputs', 'Outputs', 'Date Created']
-	var attrArr = process.attributes.map(function (a) { return a.name.replace(/"/g, '""') })
-
-	var firstRow = [ '"' + arr.concat(attrArr).join('","') + '"']
-
-	var tasksAsRows = tasks.map(function (task) {
-		return taskAsRow(process,task, attributeColumns)
-	})
-
-	var csv = tasksAsRows.join('\n')
-
-	return firstRow + '\n' + csv
-
-}
-
 function icon(k) {
 	var i = k.substr(0, k.length-4)
 	return process.env.PUBLIC_URL + "/img/" + i + "@3x.png"
 }
 
-export { display, getNotes, getOperator, getAttributesToColumnNumbers, toCSV, icon }
+export { display, icon }
