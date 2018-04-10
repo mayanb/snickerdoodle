eda8f91983072186bc9936b931f055d8c2eca681
Ishita Prasad <ishita@usepolymer.com>
2018-04-09 22:31:54 -0700

    flag shows up as red even if ancestor is flagged

--- src/components/TaskPage/TaskHeader.jsx
+++ src/components/TaskPage/TaskHeader.jsx
@@ -6,22 +6,32 @@ import { withRouter } from 'react-router'
 class TaskHeader extends React.Component {
 	render() {
 		const { task, onToggleFlag, history } = this.props
-		const isFlagged = task.is_flagged
+		const { is_flagged, ancestor_is_flagged } = task
 		const title = `Activity Log / ${task.display}`
 		const onBack = () => history.push('/activity-log')
 
 		return (
-			<div className={'task-header ' + (isFlagged ? 'flagged' : '')}>
+			<div className={'task-header ' + (is_flagged || ancestor_is_flagged ? 'flagged' : '')}>
 				<div className="title">
 					<i className="material-icons" onClick={onBack}>arrow_back</i>
 					{title}
 				</div>
+				{ ancestor_is_flagged && <TaskAncestorFlagged /> }
 				<button className="header-button" onClick={onToggleFlag}>
-					{isFlagged ? 'Remove flag' : 'Flag this task'}
+					{ is_flagged ? 'Remove flag' : 'Flag this task'}
 				</button>
 			</div>
 		)
 	}
+}
+
+function TaskAncestorFlagged(props) {
+	return (
+		<div className="task-ancestor-flag">
+			<i className="material-icons">error</i>
+			<span>This task has flagged ancestors.</span>
+		</div>
+	)
 }
 
 export default withRouter(TaskHeader)
