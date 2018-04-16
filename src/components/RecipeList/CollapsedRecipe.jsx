65c9d5f9344a1d1679cd4fca1a2b392f05c24415
David Mora <davidnmora@gmail.com>
2018-04-16 16:26:01 -0700

    Fix icons by using <Img>

--- src/components/RecipeList/CollapsedRecipe.jsx
+++ src/components/RecipeList/CollapsedRecipe.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import { formatAmount } from '../../utilities/stringutils'
-import ReactImageFallback from 'react-image-fallback'
 import ElementCard from '../Element/ElementCard'
 import './styles/recipe.css'
+import {icon} from '../TaskPage/TaskHelpers.jsx'
+import Img from '../Img/Img'
 
 export default function CollapsedRecipe(props) {
 	const stage = props.process_type.name
 	const ingredients = props.ingredients.length
 	const { process_type } = props
 	const recipeYield = formatAmount(process_type.default_amount, process_type.unit)
-	const icon = process_type.icon
+	const processIcon = process_type.icon
 	return (
 		<ElementCard className="recipe">
 			<RecipeField className="list-item-stage">
-				<RenderIconOrImage img={icon}/>
+				<Img className="icon-img" src={icon(processIcon)} />
 				<span>{stage}</span>
 			</RecipeField>
 			<RecipeField className="list-item-ingredients">{ingredients}</RecipeField>
@@ -24,15 +25,4 @@ export default function CollapsedRecipe(props) {
 
 function RecipeField({children, className}) {
 	return <span className={`recipe-item-field ${className}`}>{children}</span>
-}
-
-function RenderIconOrImage(props) {
-	var symbol = false
-	if (props.img) {
-		let errorImg = `${process.env.PUBLIC_URL}/img/default@3x.png`
-		symbol = <ReactImageFallback src={process.env.PUBLIC_URL + `/img/${props.img}@3x.png`} fallbackImage={errorImg}/>
-	} else if (props.icon) {
-		symbol = <span><i className="material-icons arrow">{props.icon}</i></span>
-	}
-	return symbol
 }
