35543433a5092584d80d5b0585b71a55e77f7fc5
Steven Guichard <steven@usepolymer.com>
2018-05-01 11:01:04 -0700

    Upgrade to v10

--- src/components/Activity/ActivityListItem.jsx
+++ src/components/Activity/ActivityListItem.jsx
@@ -5,25 +5,25 @@ import {formatAmount} from "../../utilities/stringutils"
 import Icon from '../Card/Icon'
 
 export default function ActivityListItem({ item, onClick }) {
-	const { process_code, process_name, product_code, runs, outputs, process_unit } = item
+	const { process_type, product_types, runs, amount } = item
 	
 	return (
 		<ObjectListItem className="activity-list-item" onClick={onClick}>
 			<OverflowSafeText className="process-code">
-				<Icon src="" size="20px" content={process_code}/>
-				{process_code}
+				<Icon src="" size="20px" content={process_type.code}/>
+				{process_type.code}
 			</OverflowSafeText>
 			<OverflowSafeText className="process-name">
-				{process_name}
+				{process_type.name}
 			</OverflowSafeText>
 			<OverflowSafeText className="product-code">
-				{product_code}
+				{product_types[0].code}
 			</OverflowSafeText>
 			<OverflowSafeText className="runs">
 				{formatAmount(Number(runs), 'run')}
 			</OverflowSafeText>
 			<OverflowSafeText className="outputs">
-				{formatAmount(Number(outputs), process_unit)}
+				{formatAmount(Number(amount), process_type.unit)}
 			</OverflowSafeText>
 			<div className="view-all-tasks">View all tasks</div>
 		</ObjectListItem>
