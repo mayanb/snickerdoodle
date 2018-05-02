78f9a1f608fa1a9afa55bc8bb27347fa1aea3c84
David Mora <davidnmora@gmail.com>
2018-05-02 15:27:49 -0700

    Refactor new feature dialogue (Maya told me to do it :blush: )

---
+++ src/components/NewFeatures/PageSpecificNewFeatureIntro.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Dialog from '../Card/Dialog'
+import Button from '../Card/Button'
+import Img from '../Img/Img'
+import './styles/pagespecificnewfeatureinfo.css'
+
+
+export default function PageSpecificNewFeatureIntro({ onClose, recipeContent, featureName, finalCallToAction, imgSrc, link, localStorageVarName }) {
+	const modalPreviouslySeen = window.localStorage.getItem(localStorageVarName)
+	if (modalPreviouslySeen) {
+		console.log('seen!')
+		return null
+	}
+	
+	function handleClose() {
+		window.localStorage.setItem(localStorageVarName, true)
+		onClose()
+	}
+	
+	return (
+		<Dialog onToggle={handleClose} className="new-features-card">
+			<div >
+				<div className="new-feature">
+					<div className="new-feature-header">Introducing {featureName}</div>
+					<div style={{display: "flex", alignItems: "flex-end", "justifyContent": "center"}}><Img src={imgSrc}
+																																																	height="350px"/>
+					</div>
+					<div className="new-feature-text">{recipeContent}</div>
+					<div>
+						<Button link onClick={() => window.open(link, "_blank")}>
+								<span className="learn-how">{finalCallToAction}</span>
+						</Button>
+					</div>
+				</div>
+			</div>
+		</Dialog>
+	)
+}
