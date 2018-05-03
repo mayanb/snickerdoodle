9af726e53733cfd91578cb5ffdbe3392be8105b6
Steven Guichard <steven@usepolymer.com>
2018-05-03 16:18:09 -0700

    Escape regex characters

--- src/utilities/filters.jsx
+++ src/utilities/filters.jsx
@@ -1,3 +1,8 @@
 export function processProductFilter(option, filter) {
-	return `${option.code}${option.name}`.match(new RegExp(filter, 'i'))
+	return `${option.code}${option.name}`.match(new RegExp(escapeRegExp(filter), 'i'))
+}
+
+// From https://stackoverflow.com/questions/3446170/escape-string-for-use-in-javascript-regex
+function escapeRegExp(str) {
+	return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
 }
