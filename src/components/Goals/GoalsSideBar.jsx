5f810647bd0b2dc7a8ac9fc61c84877ceea30cf2
David Mora <davidnmora@gmail.com>
2018-05-21 14:56:43 -0700

    Create Pinned/All Goals tab view

--- src/components/Goals/GoalsSideBar.jsx
+++ src/components/Goals/GoalsSideBar.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import Loading from '../OldComponents/Loading.jsx'
+import Tabs from '../Tabs/Tabs'
 import Goal from './Goal'
 import './styles/goalssidebar.css'
 import * as actions from "./GoalsActions";
 
+const PINNNED_TITLE = 'Pinned'
+const ALL_GOALS_TITLE = 'All Goals'
+
 class GoalsSideBar extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = { pinnedTabActive: false }
+		
+		this.handleTab = this.handleTab.bind(this)
+	}
 	
 	componentDidMount() {
 		let {users} = this.props
@@ -19,50 +29,80 @@ class GoalsSideBar extends React.Component {
 			return <Loading/>
 		}
 		
-		const groupedGoals = groupWeeklyAndMonthlyGoals(weeklyGoals.data, monthlyGoals.data)
-		
+		const { pinnedTabActive } = this.state
+		const groupedGoals = this.groupWeeklyAndMonthlyGoals(weeklyGoals.data, monthlyGoals.data)
+		const tabs = [
+			{title: PINNNED_TITLE, active: pinnedTabActive, img: pinnedTabActive ? 'pinblue@3x' : 'pin@3x'},
+			{title: ALL_GOALS_TITLE, active: !pinnedTabActive},
+		]
 		return (
-			<div className='goals-side-bar'>
-				{groupedGoals.map((goalGroup, i) =>
-					<Goal
-						key={i}
-						goalGroup={goalGroup}
-					/>
-				)}
+			<div className="goals-side-bar">
+				<div className="goal-side-bar-tabs">
+					<Tabs tabs={tabs} onTab={this.handleTab}/>
+				</div>
+				<div className='goals-list-wrapper'>
+					{this.displayGoals(groupedGoals)}
+				</div>
 			</div>
 		)
 	}
+	
+	handleTab(tab) {
+		if (tab.title === PINNNED_TITLE) {
+			this.setState({ pinnedTabActive: true })
+		} else {
+			this.setState({ pinnedTabActive: false })
+		}
+	}
+	
+	displayGoals(groupedGoals) {
+		if (groupedGoals.length === 0) {
+			return <div className="goals-zero-state">{`No goals ${this.state.pinnedTabActive ? PINNNED_TITLE : '' }`}</div>
+		} else {
+			return (
+				groupedGoals.map((goalGroup, i) => <Goal key={i} goalGroup={goalGroup}/>
+				)
+			)
+		}
+	}
+	
+	groupWeeklyAndMonthlyGoals(weeklyGoals, monthlyGoals) {
+		const groupedGoals = {}
+		const { pinnedTabActive } = this.state
+		
+		// Process weekly goals
+		weeklyGoals.forEach(weeklyGoal => {
+			if (!pinnedTabActive || weeklyGoal.isPinned) {
+				const productIds = getProductIds(weeklyGoal)
+				groupedGoals[productIds] = {
+					process_type: weeklyGoal.process_type,
+					productIds: productIds,
+					weeklyGoal: weeklyGoal,
+				}
+			}
+		})
+		
+		// Add in monthly goals, combining when equal
+		monthlyGoals.forEach(monthlyGoal => {
+			if (!pinnedTabActive || monthlyGoal.isPinned) {
+				const productIds = getProductIds(monthlyGoal)
+				const weeklyGoalGroup = groupedGoals[productIds]
+				if (weeklyGoalGroup && weeklyGoalGroup.process_type === monthlyGoal.process_type) {
+					weeklyGoalGroup.monthlyGoal = monthlyGoal
+				} else {
+					groupedGoals[productIds] = { // Also consolidates any duplicate goals
+						monthlyGoal: monthlyGoal,
+						// No need to store other info (not needed to verify matches)
+					}
+				}
+			}
+		})
+		
+		return Object.values(groupedGoals)
+	}
 }
 
-const groupWeeklyAndMonthlyGoals = (weeklyGoals, monthlyGoals) => {
-	const groupedGoals = {}
-	
-	// Process weekly goals
-	weeklyGoals.forEach(weeklyGoal => {
-		const productIds = getProductIds(weeklyGoal)
-		groupedGoals[productIds] = {
-			process_type: weeklyGoal.process_type,
-			productIds: productIds,
-			weeklyGoal: weeklyGoal,
-		}
-	})
-	
-	// Add in monthly goals, combining when equal
-	monthlyGoals.forEach(monthlyGoal => {
-		const productIds = getProductIds(monthlyGoal)
-		const weeklyGoalGroup = groupedGoals[productIds]
-		if (weeklyGoalGroup && weeklyGoalGroup.process_type === monthlyGoal.process_type) {
-			weeklyGoalGroup.monthlyGoal = monthlyGoal
-		} else {
-			groupedGoals[productIds] = { // Also consolidates any duplicate goals
-				monthlyGoal: monthlyGoal,
-				// No need to store other info (not needed to verify matches)
-			}
-		}
-	})
-	
-	return Object.values(groupedGoals)
-}
+
 
 // Returns a unique process/product(s) identifier used as the key of the hashmap to group weekly/monthly goals
 const getProductIds = (goal) => {
