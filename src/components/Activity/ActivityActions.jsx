1967f1743dbda3a7c830e06696ba1d59bc4becd2
Steven Guichard <steven@usepolymer.com>
2018-05-22 11:45:38 -0700

    Refactor download actions to separate reusable file

--- src/components/Activity/ActivityActions.jsx
+++ src/components/Activity/ActivityActions.jsx
@@ -5,10 +5,9 @@ import {
 	REQUEST_FAILURE,
 	PAGE,
 } from '../../reducers/APIDataReducer'
-import { ACTIVITY, CSV, GOOGLE_SHEET } from '../../reducers/ReducerTypes'
-import fileDownload from 'js-file-download'
+import { ACTIVITY } from '../../reducers/ReducerTypes'
+import * as downloadActions from '../../reducers/DownloadActions'
 import { dateToUTCString } from '../../utilities/dateutils'
-import { isDandelion } from '../../utilities/userutils'
 
 export function fetchActivity(params) {
 	return function (dispatch) {
@@ -73,101 +72,19 @@ function csvTitle(filters, processes) {
 	return `${name} - ${start}-${end}`
 }
 
-function fetchCsv(filters, user, processes) {
-	return function (dispatch) {
-		const params = createParams(filters, user.user_id)
-		const title = csvTitle(filters, processes)
-		dispatch(requestCsv())
-		return api.post('/gauth/create-csv/')
-			.type('form')
-			.send(params)
-			.responseType('blob')
-			.then(res => {
-				dispatch(requestCsvSuccess())
-				return fileDownload(res.body, `${title}.csv`)
-			})
-			.catch(err => dispatch(requestCsvFailure(err)))
-	}
-}
-
-function requestCsv() {
-	return {
-		type: REQUEST,
-		name: CSV
-	}
-}
-
-function requestCsvFailure(err) {
-	console.error('Oh no! Something went wrong\n' + err)
-	return {
-		type: REQUEST_FAILURE,
-		name: CSV
-	}
-}
-
-function requestCsvSuccess() {
-	return {
-		type: REQUEST_SUCCESS,
-		name: CSV,
-		data: null,
-	}
-}
-
-function fetchGoogleSheet(filters, user, toggleDialog) {
-	return function (dispatch) {
-		const params = createParams(filters, user.user_id)
-		if (!user.gauth_access_token) {
-			toggleDialog('mustConnectGoogleDialog')
-			return dispatch(requestGoogleSheetFailure('Must connect Google Account'))
-		}
-		dispatch(requestGoogleSheet())
-		return api.post('/gauth/create-spreadsheet/')
-			.type('form')
-			.send(params)
-			.then(res => {
-				dispatch(requestGoogleSheetSuccess(res.body))
-				let url = 'https://docs.google.com/spreadsheets/d/' + res.body.spreadsheetId + '/'
-				let newWin = window.open(url, '_blank');
-				if (!newWin || newWin.closed || typeof newWin.closed === 'undefined') {
-					//POPUP BLOCKED
-					toggleDialog('mustEnablePopupsDialog')
-				}
-				return res
-			})
-			.catch(err => dispatch(requestGoogleSheetFailure(err)))
-	}
-}
-
-function requestGoogleSheet() {
-	return {
-		type: REQUEST,
-		name: GOOGLE_SHEET
-	}
-}
-
-function requestGoogleSheetFailure(err) {
-	console.error('Oh no! Something went wrong\n' + err)
-	return {
-		type: REQUEST_FAILURE,
-		name: GOOGLE_SHEET
-	}
-}
-
-function requestGoogleSheetSuccess() {
-	return {
-		type: REQUEST_SUCCESS,
-		name: GOOGLE_SHEET,
-		data: null,
-	}
-}
-
 export function fetchDownload(filters, user, processes, toggleDialog) {
-	const team = user.team_name
-	if (isDandelion(team)) {
-		return fetchCsv(filters, user, processes)
-	} else {
-		return fetchGoogleSheet(filters, user, toggleDialog)
+	const params = createParams(filters, user.user_id)
+	const title = csvTitle(filters, processes)
+	const csvRequest = {
+		url: '/gauth/create-csv/',
+		query: params,
+		title: title,
 	}
+	const googleRequest = {
+		url: '/gauth/create-spreadsheet/',
+		query: params,
+	}
+	return downloadActions.fetchDownload(csvRequest, googleRequest, user, toggleDialog)
 }
 
 export function pageActivity(direction) {
