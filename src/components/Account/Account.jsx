93159c4d1a0ce2af16fe5c00c7fe6f553817ae2e
David Mora <davidnmora@gmail.com>
2018-05-31 15:04:57 -0700

    On label image click, save option to DB

--- src/components/Account/Account.jsx
+++ src/components/Account/Account.jsx
@@ -6,18 +6,27 @@ import AccountIntegrations from './AccountIntegrations'
 import FactoryOptions from './FactoryOptions'
 import AccountTeam from './AccountTeam'
 import './styles/account.css'
+import * as actions from "../AccountMenu/UserActions";
 
 class Account extends React.Component {
+	constructor(props) {
+		super(props)
+		this.updateFactorySetting = this.updateFactorySetting.bind(this)
+	}
 	render() {
 		return (
 			<div className="my-account">
 				<AccountHeader />
 				<AccountBasics />
 				<AccountIntegrations ext={this.props.match.params.ext} />
-				<FactoryOptions taskLabelType={this.props.taskLabelType}/>
+				<FactoryOptions taskLabelType={this.props.user.task_label_type} onSubmit={this.updateFactorySetting}/>
 				<AccountTeam />
 			</div>
 		)
+	}
+	
+	updateFactorySetting(key, value) {
+		this.props.dispatch(actions.updateUserSetting(this.props.user.team, key, value))
 	}
 }
 
@@ -26,12 +35,10 @@ const mapStateToProps = (state/*, props*/) => {
 	const user = data[ui.activeUser].user
   return {
     users: state.users,
-		taskLabelType: user.task_label_type,
+		user: user,
   }
 }
 
 const connected = connect(mapStateToProps)(Account)
 
 export default connected
-
-// <AccountIntegrations ext={this.props.match.params.ext}/>
--- src/components/Account/FactoryOptions.jsx
+++ src/components/Account/FactoryOptions.jsx
@@ -3,44 +3,41 @@ import AccountSectionHeader from './AccountSectionHeader'
 import Card from '../Card/Card'
 import Img from '../Img/Img'
 
-
-export default function FactoryOptions({ taskLabelType }) {
+export default function FactoryOptions({ taskLabelType, onSubmit }) {
 	return (
 			<div className="factory-options">
 				<AccountSectionHeader title="Factory Options" />
-				<LabelOptions taskLabelType={taskLabelType}/>
-			</div>
-	)
-}
-
-function LabelOptions({ taskLabelType }) {
-	return (
-		<Card>
-			<div className="account-integration">
-				<div className="integration-info">
-					<div>
-						<span>Printed Label Style</span>
+				<Card>
+					<div className="account-integration">
+						<div className="integration-info">
+							<div>
+								<span>Printed Label Style</span>
+							</div>
+						</div>
+						<div className="integration-detail">
+							<span>Click to select the style of label you'd like printed whenever tasks labels are printed on the mobile app.</span>
+						</div>
+						<SelectableLabelImages taskLabelType={taskLabelType} onSubmit={onSubmit}/>
 					</div>
-				</div>
-				<div className="integration-detail">
-					<span>Click to select the style of label you'd like printed whenever tasks labels are printed on the mobile app.</span>
-				</div>
-				<SelectableLabelImages taskLabelType={taskLabelType}/>
+				</Card>
 			</div>
-		</Card>
 	)
 }
 
-function SelectableLabelImages({ taskLabelType }) {
+function SelectableLabelImages({ taskLabelType, onSubmit }) {
 	const labelImages = ['goal-setting', 'goal-setting', 'goal-setting']
-	console.log(taskLabelType)
 	return (
 		<div className="selectable-label-images">
 			{labelImages.map((img, i) =>
-				<div key={i} className={`label-image ${taskLabelType === i ? 'selected' : ''}`}>
+				<div
+					key={i}
+					className={`label-image ${taskLabelType === i ? 'selected' : ''}`}
+					onClick={() => onSubmit('task_label_type', i)}
+				>
 					<Img src={img} className="icon"/>
 				</div>
 			)}
 		</div>
 	)
 }
+
