871399badced074f3b8a8acc9fe79d46841f932c
Matt Graham <mpgraham917@gmail.com>
2018-08-27 11:58:27 -0700

    Completed Production Aside with goals

--- src/components/ProductionPlanning/ProductionAside.jsx
+++ src/components/ProductionPlanning/ProductionAside.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-import { Progress } from 'antd';
+import { connect } from 'react-redux'
 import Img from '../Img/Img'
+import * as goalActions from '../Goals/GoalsActions'
 import './styles/productionaside.css'
+import { formatAmount } from '../../utilities/stringutils'
+import * as goalUtils from '../Goals/GoalUtils'
 
-export class ProductionAside extends React.Component {
+class ProductionAside extends React.Component {
 	constructor(props) {
 		super(props)
 		let mockData = [
@@ -48,35 +51,39 @@ export class ProductionAside extends React.Component {
 				warning: false
 			},
 		]
-
+		
+		this.props.dispatch(goalActions.fetchGoals())
 		this.state = {
 			data: mockData
 		}
 	}
 
-	componentDidMount() {
-
-	}
-
 	render() {
-		const { data } = this.state
+		const { goals } = this.props
 		return (
 			<div className='production-aside-container'>
 				<div className='title'>In Production this Month</div>
-				{ data && data.map(item => this.renderItem(item)) }
+				{ goals && goals.map(groupedGoal => this.renderGoal(groupedGoal)) }
 			</div>
 		)
 	}
 
-	renderItem(item) {
-		const { process, product, amount_remaining, monthly_goal, daily_goal, warning } = item
-		const percent = monthly_goal.created / monthly_goal.goal * 100
+	renderGoal(groupedGoal) {
+		const { info, w: weeklyGoal, m: monthlyGoal } = groupedGoal
+		const { process_name, process_unit, product_code, process_id, product_id, inventory_amount } = info
+		const warning = null // need to fetch if there are any raw materials running low for this Process/Product type
+		let percent = 0
+		if (monthlyGoal) {
+			percent = monthlyGoal.actual / monthlyGoal.goal * 100
+		} else if (weeklyGoal) {
+			percent = weeklyGoal.actual / weeklyGoal.goal * 100
+		}
 		return (
-			<div className='item-container' key={`${process} ${product}`}>
-				<Progress strokeLinecap='square' showInfo={false} percent={percent} style={{marginTop: 'none'}}/>
+			<div className='goal-container' key={`${process_name} ${product_code}`} onClick={() => this.handleSelect(process_id, product_id)}>
+				<ProgressBar percent={percent} />
 				<div className='content-container'>
 					<div className='title-container'>
-						<div className='title'>{`${process} ${product}`}</div>
+						<div className='goal-title'>{`${process_name} ${product_code}`}</div>
 
 						{ warning && 
 							<div className='warning'>
@@ -86,22 +93,68 @@ export class ProductionAside extends React.Component {
 					</div>
 					<div className='info-container'>
 						<div className='label'>In Inventory</div>
-						<div className='value'>{amount_remaining}</div>
+						<div className='value'>{formatAmount(inventory_amount, process_unit)}</div>
 					</div>
+					{ monthlyGoal && 
 					<div className='info-container'>
 						<div className='label'>Monthly Goal</div>
-						<div className='value'>{`${monthly_goal.created}/${monthly_goal.goal}`}</div>
+						<div className='value'>{`${monthlyGoal.actual ? monthlyGoal.actual : 0}/${monthlyGoal.goal}`}</div>
 					</div>
+					}
+					{ weeklyGoal &&
 					<div className='info-container'>
-						<div className='label'>Daily Goal</div>
-						<div className='value'>{daily_goal}</div>
+						<div className='label'>Weekly Goal</div>
+						<div className='value'>{`${weeklyGoal.actual ? weeklyGoal.actual : 0}/${weeklyGoal.goal}`}</div>
 					</div>
+					}
 				</div>
 			</div>
 		)
 	}
 
-	handleSelectRow(i) {
-		//this.props.dispatch(actions.selectInventory(i))
+	handleSelect(process_id, product_id) {
+		this.props.onSelect(String(process_id), String(product_id))
 	}
 }
+
+function ProgressBar({ percent }) {
+	return (
+		<div className='outer-progress-bar'>
+			<div className='inner-progress-bar' style={{ width: `${percent}%` }}></div>
+		</div>
+	)
+}
+
+function formatGoals(goals) {
+	const goalGroups = {}
+	goals.forEach(goal => {
+		if (!goal.is_trashed && !goal.all_product_types) {
+			const goalKey = goalUtils.getProductProcessKey(goal)
+			if (!goalGroups[goalKey]) {
+				goalGroups[goalKey] = {}
+			}
+			goalGroups[goalKey].info = { 
+				inventory_amount: goal.total_inventory_amount,
+				process_id: goal.process_type,
+				process_name: goal.process_name, 
+				process_unit: goal.process_unit,
+				product_id: goal.product_code[0].id,
+				product_code: goal.product_code[0].code,
+			}
+			goalGroups[goalKey][goal.timerange] = { 
+				actual: Math.round(goal.actual), 
+				goal: Math.round(goal.goal) 
+			}
+		}
+	})
+	return Object.values(goalGroups)
+}
+
+const mapStateToProps = (state/*, props*/) => {
+	const goals = formatGoals(state.goals.data)
+	return {
+		goals,
+	}
+}
+
+export default connect(mapStateToProps)(ProductionAside)
