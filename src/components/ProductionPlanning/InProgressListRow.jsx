accfc3f1015176d42e471e296e2e92ef5931e828
Matt Graham <mpgraham917@gmail.com>
2018-08-29 20:01:05 -0700

    Add Category Tag :cat:

--- src/components/ProductionPlanning/InProgressListRow.jsx
+++ src/components/ProductionPlanning/InProgressListRow.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { formatAmount, getProcessIcon } from "../../utilities/stringutils"
 import ObjectListItem from '../ObjectList/ObjectListItem'
 import OverflowSafeText from '../OverflowSafeText/OverflowSafeText'
+import CategoryTag from '../CategoryTag/CategoryTag'
 import Img from '../Img/Img'
 import './styles/inprogresslistrow.css'
 
@@ -19,7 +20,7 @@ export default function InProgressListRow({item, onClick, isSelected, context}) 
 					{inventoryCodeName(process_type, product_type)}
 				</OverflowSafeText>
 			</div>
-			<div className='inv-col inv-category'>{process_type.category /*<CategoryTag category={process_type.category} />*/}</div>
+			<div className='inv-col inv-category'><CategoryTag category={process_type.category} /></div>
 			<div className='inv-col inv-in-stock'>{formatAmount(adjusted_amount, process_type.unit)}</div>
 			<div className='inv-col inv-can-make'>{formatAmount(can_make, selectedProcess.unit)}</div>
 		</ObjectListItem>
