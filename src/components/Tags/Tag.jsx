29bc2cafba813d5a83c2d69c67097bf90494ccc7
Matt Graham <mpgraham917@gmail.com>
2018-09-10 10:53:00 -0700

    Changed tags to have colors that are more contrastable with white text

--- src/components/Tags/Tag.jsx
+++ src/components/Tags/Tag.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Tag as AntdTag } from 'antd'
-import { colorHash } from '../../utilities/stringutils'
+import { tagColorHash } from '../../utilities/stringutils'
 
 export default function Tag({style, name, ...rest}) {
     return (
         <div className='tag'>
-            <AntdTag color={colorHash(name)} style={{textAlign:'left', fontSize:'11px', ...style}} {...rest}>
+            <AntdTag color={tagColorHash(name)} style={{textAlign:'left', fontSize:'11px', ...style}} {...rest}>
                 {name}
             </AntdTag>
         </div>
