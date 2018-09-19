90f0aeac18b0a3f2d553b0c021d6ed65664d53a2
David Mora <davidnmora@gmail.com>
2018-09-19 09:58:37 -0700

    Fetch flagged tasks directly from delimited string

--- src/components/TaskPage/TaskHeader.jsx
+++ src/components/TaskPage/TaskHeader.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import './styles/taskheader.css'
 import { withRouter } from 'react-router'
 import TaskDialogSimple from '../TaskDialog/TaskDialogSimple'
+import api from '../WaffleconeAPI/api'
 
 
 class TaskHeader extends React.Component {
@@ -10,7 +11,6 @@ class TaskHeader extends React.Component {
 		const { task, onToggleFlag, history } = this.props
 		const { is_flagged, flagged_ancestors_id_string } = task
 		const title = `Activity Log / ${task.display}`
-		const flagged_ancestors = (this.props.ancestors || []).filter(e => e.is_flagged)
 		const onBack = () => history.push('/activity-log')
 
 		return (
@@ -19,7 +19,7 @@ class TaskHeader extends React.Component {
 					<i className="material-icons" onClick={onBack}>arrow_back</i>
 					{title}
 				</div>
-				{ flagged_ancestors_id_string && <TaskAncestorFlagged tasks={flagged_ancestors}/> }
+				{ flagged_ancestors_id_string && <TaskAncestorFlagged flagged_ancestors_id_string={flagged_ancestors_id_string}/> }
 				<button className="header-button" onClick={onToggleFlag}>
 					{ is_flagged ? 'Remove flag' : 'Flag this task'}
 				</button>
@@ -33,8 +33,21 @@ class TaskAncestorFlagged extends React.Component {
 		super(props)
 		this.state = {
 			dialog: false,
+			flagged_tasks: [],
 		}
 		this.handleToggle = this.handleToggle.bind(this)
+	}
+	
+	componentDidMount() {
+		const { flagged_ancestors_id_string } = this.props
+		const params = { flagged_ancestors_id_string: flagged_ancestors_id_string }
+		
+		// Rather than wait the potentially SUPER LONG time to retrieve all ancestors and filter for flagged ones
+		// we instead use the delimited string of flagged task ids to retrieve flagged tasks separately and quickly.
+		api.get('/ics/tasks/')
+			.query(params)
+			.then(res => this.setState({ flagged_tasks: res.body }))
+			.catch(err => console.log(err))
 	}
 
 	handleToggle() {
@@ -42,9 +55,11 @@ class TaskAncestorFlagged extends React.Component {
 	}
 
 	render() {
+		const { flagged_tasks } = this.state
+
 		return (
 			<div className="task-ancestor-flag">
-				{ this.state.dialog && <TaskDialogSimple header='Flagged ancestors' onToggle={this.handleToggle} tasks={this.props.tasks}/> }
+				{ this.state.dialog && <TaskDialogSimple header='Flagged ancestors' onToggle={this.handleToggle} tasks={flagged_tasks}/> }
 				<i className="material-icons">error</i>
 				<span>This task has flagged ancestors.&nbsp;<span className="flagged-ancestors-review" onClick={this.handleToggle}>Review them now.</span></span>
 			</div>
