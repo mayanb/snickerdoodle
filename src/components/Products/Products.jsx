32cd3266d18068cd5de344252b7bedc07e778c6b
Matt Graham <mpgraham917@gmail.com>
2018-09-20 11:13:48 -0700

    Indendation was all wonky

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -12,42 +12,41 @@ import CreateProductDialog from './CreateProductDialog'
 import ApplicationSectionHeaderWithButton from '../Application/ApplicationSectionHeaderWithButton'
 import ElementFilter from '../Element/ElementFilter'
 import './styles/products.css'
-import PageSpecificNewFeatureIntro from '../NewFeatures/PageSpecificNewFeatureIntro'
 
 const { confirm } = Modal
 
 class Products extends React.Component {
-  constructor(props) {
-    super(props)
+	constructor(props) {
+		super(props)
 
-	  this.state = {
-		  isAddingProduct: false,
-		  filter: null,
-		  ordering: 'name',
-	  }
+		this.state = {
+			isAddingProduct: false,
+			filter: null,
+			ordering: 'name',
+		}
 
-	  this.renderHeaderRow = this.renderHeaderRow.bind(this)
+		this.renderHeaderRow = this.renderHeaderRow.bind(this)
 
-	  this.handleFilter = this.handleFilter.bind(this)
-	  this.handleToggleDialog = this.handleToggleDialog.bind(this)
-    this.handlePagination = this.handlePagination.bind(this)
-    this.handleCreateProduct = this.handleCreateProduct.bind(this)
-	  this.handleArchive = this.handleArchive.bind(this)
-	  this.handleSelect = this.handleSelect.bind(this)
-	  this.handleReorder = this.handleReorder.bind(this)
-  }
+		this.handleFilter = this.handleFilter.bind(this)
+		this.handleToggleDialog = this.handleToggleDialog.bind(this)
+		this.handlePagination = this.handlePagination.bind(this)
+		this.handleCreateProduct = this.handleCreateProduct.bind(this)
+		this.handleArchive = this.handleArchive.bind(this)
+		this.handleSelect = this.handleSelect.bind(this)
+		this.handleReorder = this.handleReorder.bind(this)
+  	}
 
   // fetch products on load
-  componentDidMount() {
-  	this.fetchProducts()
-    this.props.dispatch(recipeActions.fetchRecipes())
-  }
+	componentDidMount() {
+		this.fetchProducts()
+		this.props.dispatch(recipeActions.fetchRecipes())
+	}
 
 	fetchProducts() {
 		const { ordering, filter } = this.state
 		const params = {}
 		if (ordering) {
-  		params['ordering'] = ordering
+  			params['ordering'] = ordering
 		}
 		if (filter) {
 			params['filter'] = filter
@@ -55,60 +54,63 @@ class Products extends React.Component {
 		this.props.dispatch(actions.fetchProducts(params))
 	}
 
-  render() {
-    let { users, ui, data, recipeUI, recipeData } = this.props
-    let account_type = users.data[users.ui.activeUser].user.account_type
-    if (account_type !== 'a') {
-    	this.props.history.push('/')
-    }
+  	render() {
+		let { users, ui, data } = this.props
+		let account_type = users.data[users.ui.activeUser].user.account_type
+		if (account_type !== 'a') {
+			this.props.history.push('/')
+		}
 
-    let hasNone = !ui.isFetchingData && (!data || !data.length) && !this.state.filter
-	  return (
-	  	<div className="products">
-			  <ApplicationSectionHeaderWithButton onToggleDialog={this.handleToggleDialog} buttonText="Create product"
-			                                      title="Products" />
-					{ hasNone ? <ZeroState type="product" /> : this.renderTable() }
-			 		{this.renderDialog()}
-		  </div>
-	  )
-  }
-
-  renderTable() {
-  	return (
-  		<div>
-  			<ElementFilter onChange={this.handleFilter} className="product-filter" />
-	  		<ObjectList className="products" isFetchingData={this.props.ui.isFetchingData}>
-				  <PaginatedTable
-					  {...this.props}
-					  onClick={this.handleSelect}
-					  onPagination={this.handlePagination}
-					  Row={ProductsListItem}
-					  TitleRow={this.renderHeaderRow}
-					  extra={{onArchive: this.handleArchive}}
-				  />
-			  </ObjectList>
-		  </div>
+		let hasNone = !ui.isFetchingData && (!data || !data.length) && !this.state.filter
+		return (
+			<div className="products">
+				<ApplicationSectionHeaderWithButton 
+					onToggleDialog={this.handleToggleDialog} 
+					buttonText="Create product"
+					title="Products" 
+				/>
+				{ hasNone ? <ZeroState type="product" /> : this.renderTable() }
+				{this.renderDialog()}
+			</div>
 		)
-  }
+  	}
 
-  renderDialog() {
-	  return (
-		  <CreateProductDialog
-			  isOpen={this.state.isAddingProduct}
-			  onToggle={this.handleToggleDialog}
-			  onCreate={this.handleCreateProduct}
-		  />
-	  )
-  }
+	renderTable() {
+		return (
+			<div>
+				<ElementFilter onChange={this.handleFilter} className="product-filter" />
+				<ObjectList className="products" isFetchingData={this.props.ui.isFetchingData}>
+					<PaginatedTable
+						{...this.props}
+						onClick={this.handleSelect}
+						onPagination={this.handlePagination}
+						Row={ProductsListItem}
+						TitleRow={this.renderHeaderRow}
+						extra={{onArchive: this.handleArchive}}
+					/>
+				</ObjectList>
+			</div>
+		)
+	}
+
+	renderDialog() {
+		return (
+			<CreateProductDialog
+				isOpen={this.state.isAddingProduct}
+				onToggle={this.handleToggleDialog}
+				onCreate={this.handleCreateProduct}
+			/>
+		)
+	}
 
 	renderHeaderRow() {
-  	const columns = [
-		  { title: 'Code', className: 'code', field: 'code' },
-		  { title: 'Name', className: 'name', field: 'name' },
-		  { title: 'Owner', className: 'owner', field: null },
-		  { title: 'Date Created', className: 'date', field: 'created_at' },
-		  { title: null, className: 'more-options-button', field: null },
-	  ]
+		const columns = [
+			{ title: 'Code', className: 'code', field: 'code' },
+			{ title: 'Name', className: 'name', field: 'name' },
+			{ title: 'Owner', className: 'owner', field: null },
+			{ title: 'Date Created', className: 'date', field: 'created_at' },
+			{ title: null, className: 'more-options-button', field: null },
+		]
 		return (
 			<ObjectListHeader
 				columns={columns}
@@ -122,29 +124,29 @@ class Products extends React.Component {
 		this.setState({ordering: ordering}, this.fetchProducts)
 	}
 
-  /* EVENT HANDLERS */
-  handleFilter(filterText) {
-	  this.setState({ filter: filterText }, this.fetchProducts)
-  }
+	/* EVENT HANDLERS */
+	handleFilter(filterText) {
+		this.setState({ filter: filterText }, this.fetchProducts)
+	}
 
-  handlePagination(direction) {
-    this.props.dispatch(actions.pageProducts(direction))
-  }
+	handlePagination(direction) {
+		this.props.dispatch(actions.pageProducts(direction))
+	}
 
 	handleToggleDialog() {
 		this.setState({isAddingProduct: !this.state.isAddingProduct})
 	}
 
-  handleCreateProduct(json) {
-    this.props.dispatch(actions.postCreateProduct(json))
-	    .then(res => {
-		    const index = this.props.data.findIndex(e => e.id === res.item.id)
-		    return this.handleSelect(index)
-	    })
-  }
+	handleCreateProduct(json) {
+		this.props.dispatch(actions.postCreateProduct(json))
+			.then(res => {
+				const index = this.props.data.findIndex(e => e.id === res.item.id)
+				return this.handleSelect(index)
+			})
+	}
 
-  handleSelect(index) {
-  	this.props.history.push('/products/' + this.props.data[index].id)
+	handleSelect(index) {
+		this.props.history.push('/products/' + this.props.data[index].id)
 	}
 
 	handleArchive(index) {
@@ -166,19 +168,18 @@ class Products extends React.Component {
 				message.error(`Oops! We couldn't delete ${p.name} (${p.code}). Try again later.`)
 			})
 	}
-
 }
 
 // This is our select function that will extract from the state the data slice we want to expose
 // through props to our component.
 const mapStateToProps = (state/*, props*/) => {
-  return {
-    data: state.products.data,
-    ui: state.products.ui,
-    users: state.users,
-    recipeData: state.recipes.data,
-    recipeUI: state.recipes.ui
-  }
+	return {
+		data: state.products.data,
+		ui: state.products.ui,
+		users: state.users,
+		recipeData: state.recipes.data,
+		recipeUI: state.recipes.ui
+	}
 }
 
 const connectedProducts = connect(mapStateToProps)(Products)
